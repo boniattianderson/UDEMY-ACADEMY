12c30fbf9eaec8dc408df942e42b5647dc7787c4
Anderson <73034734+boniattianderson@users.noreply.github.com>
2023-03-16 08:56:32 -0300

    Add files via upload
    Duas maneiras de fazer a mesma coisa em Python

---
+++ Aula1.py
@@ -0,0 +1,13 @@
+nome = 'Anderson'
+altura = 1.80
+peso = 100
+imc = peso / (altura ** 2)
+#print('nome:',nome)
+#print('altura:', altura)
+#print('imc:', imc)
+linha_1 = f'{nome}'
+linha_2 = f'{altura:.2f}'
+linha_3 = f'{imc:.2f}'
+print(linha_1)
+print(linha_2)
+print(linha_3)
